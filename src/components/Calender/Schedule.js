7f3003dba1a096b0a1972fed238b55eb40cd3e62
optshj <optshj@gmail.com>
2023-11-24 13:39:08 +0000

    [Feat] 스케줄이 4개 넘어가면 +N개로 표시 구현

--- src/components/Calender/Schedule.js
+++ src/components/Calender/Schedule.js
@@ -4,14 +4,15 @@ import { useTodoState } from '../../context';
 
 function Schedule({day}){
 	const todos = useTodoState();
-	
+	const maxSchedule = 4;
+	const todayTodo = todos.filter(todo=>(todo.date === day.toDateString()));
+	const schedules = todayTodo.map(todo=>(<div className={todo.done ? style.done:style.schedule} style={{'backgroundColor':todo.color}} key={todo.id}>
+					{todo.text}</div>));
+	console.log(schedules);
 	return(
 		<div className={style.form}>
-			{todos.map(todo=>(
-				todo.date === day.toDateString()
-				? <div className={todo.done ? style.done:style.schedule} style={{'backgroundColor':todo.color}} key={todo.id}>
-					{todo.text}
-				</div>:null))}
+			{schedules.slice(0,4)}
+			<div className={schedules.length > maxSchedule ? style.overschedule : style.hide}>+{schedules.length-maxSchedule}</div>
 		</div>
 	)
 }
